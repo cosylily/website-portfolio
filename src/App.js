cfed32ebf5e03ed7bce1c396cbba0127c6b74918
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-09 12:03:43 +0800

    update app.js and app.css
    to recap on the missing knowledge after so long T_T

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <heeader className="title">
-        <div>
-          <h2>Syafiqah's</h2>
-          <h2>Portfolio</h2>
-        </div>
-
-        <div>
-          <img src="logoforportfolio.jpg" alt="own doodle for my logo" />
-        </div>
-      </heeader>
+      <header className="title">
+        <h2>Syafiqah's</h2>
+        <h2>Portfolio</h2>
+      </header>
+      <div>
+        <img src="logoforportfolio.jpg" alt="Self-doodle logo" />
+      </div>
     </div>
   );
 }
