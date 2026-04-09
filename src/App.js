29a7e3d39c0dc0d7aa1b530c3a8770925e9b3d96
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-09 13:17:45 +0800

    update app.css and app.js
    Trying to follow my uncertain visualisation. But the structure is there ><

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ function App() {
     <div className="App">
       <header className="title">
         <h2>Syafiqah's</h2>
+        <div>
+          <img
+            src="logoforportfolio.jpg"
+            alt="Self-doodle logo"
+            height="100px"
+            width="auto"
+          />
+        </div>
         <h2>Portfolio</h2>
       </header>
-      <div>
-        <img src="logoforportfolio.jpg" alt="Self-doodle logo" />
+      <div className="canvas">
+        <div className="buttons">
+          <button>About</button>
+          <button>Education</button>
+          <button>Certificate</button>
+          <button>List of Websites</button>
+        </div>
+        <div className="emptyspace"></div>
       </div>
     </div>
   );
