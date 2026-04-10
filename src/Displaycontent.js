654da1c87f305b38ece899a57dbe21ef8aa51ab7
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-10 17:10:18 +0800

    added Showcertificates.js

--- src/Displaycontent.js
+++ src/Displaycontent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "./About.js";
 import Education from "./Education.js";
+import Showcerts from "./Showcertificates.js";
 
 export default function DisplayContent(props) {
   if (props.name === "about") {
@@ -8,6 +9,9 @@ export default function DisplayContent(props) {
   }
   if (props.name === "education") {
     return <Education />;
+  }
+  if (props.name === "certificates") {
+    return <Showcerts />;
   } else {
     return <div>Click any button to start!</div>;
   }
---
+++ src/Showcertificates.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Certs() {
+  return (
+    <div>
+      <h2>Certifications of Coding Skills</h2>
+    </div>
+  );
+}
