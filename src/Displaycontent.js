05db9fcbc9159fd1ebc375c427172da94dbf0b6a
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-10 17:02:10 +0800

    Added education.js

--- src/Displaycontent.js
+++ src/Displaycontent.js
@@ -1,12 +1,13 @@
 import React from "react";
 import About from "./About.js";
+import Education from "./Education.js";
 
 export default function DisplayContent(props) {
   if (props.name === "about") {
     return <About />;
   }
   if (props.name === "education") {
-    return <div>education</div>;
+    return <Education />;
   } else {
     return <div>Click any button to start!</div>;
   }
---
+++ src/Education.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Education() {
+  return (
+    <div className="educationsection">
+      <h2>Education</h2>
+      <p>
+        My degree is Politics, Philosophy and Economics but I gained this skill
+        out of interest.{" "}
+      </p>
+    </div>
+  );
+}
