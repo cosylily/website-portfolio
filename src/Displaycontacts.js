6fa61b697d00fcf710a3355a7f4b7a341bbf7fa8
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-13 19:13:56 +0800

    update Displaycontacts.js
    Add contacts (doing the easy stuff first =] )

--- src/Displaycontacts.js
+++ src/Displaycontacts.js
@@ -1,11 +1,19 @@
 import React from "react";
+import "./App.css";
 
 export default function Contacts() {
   return (
     <div>
       <h2>Contact for work</h2>
       <ul>
-        <li>Email: syafiqah.dayana1205@gmail.com</li>
+        <li>
+          Email:{"  "}
+          <a href="mailto:syafiqah.dayana1205@gmial.com">
+            syafiqah.dayana1205@gmail.com
+          </a>
+        </li>
+        <li>Instagram: </li>
+        <li>XiaoHongShu: </li>
       </ul>
     </div>
   );
