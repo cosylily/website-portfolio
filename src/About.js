534865039f9fe08f6ea380feec9d644f2385cb27
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-13 19:47:09 +0800

    update about.js
    added in some descriptions

--- src/About.js
+++ src/About.js
@@ -4,7 +4,17 @@ export default function About() {
   return (
     <div className="aboutSection">
       <h2>About Me</h2>
-      <p>This is testing only. Not yet think of about content!</p>
+      <h3>Introduction:</h3>
+      <p>
+        Hello World! My name is Syafiqah, an aspiring freelance website designer
+        from Malaysia. I graduated from University of Manchester under the
+        degree of Politics, Philosophy and Economics and had the chance to take
+        an online course on website development. After taking the online course
+        for one year, I have mastered HTML, CSS, JavaScript and React,js. I
+        enjoy the creative process of designing websites, aiming at capturing
+        audiences' visual(thinking something like designing stuff to attract
+        their vision.)
+      </p>
     </div>
   );
 }
