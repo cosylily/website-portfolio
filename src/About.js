273cd9b3ba6d4f32b7228fd92231000b633ab170
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-14 13:15:28 +0800

    update About.js
    Added some contents in this section and changed the text alignment for p to be at start.

--- src/About.js
+++ src/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 
 export default function About() {
   return (
@@ -6,14 +7,112 @@ export default function About() {
       <h2>About Me</h2>
       <h3>Introduction:</h3>
       <p>
-        Hello World! My name is Syafiqah, an aspiring freelance website designer
-        from Malaysia. I graduated from University of Manchester under the
-        degree of Politics, Philosophy and Economics and had the chance to take
-        an online course on website development. After taking the online course
-        for one year, I have mastered HTML, CSS, JavaScript and React,js. I
-        enjoy the creative process of designing websites, aiming at capturing
-        audiences' visual(thinking something like designing stuff to attract
-        their vision.)
+        Hello World! My name is Syafiqah, an aspiring junior website developer
+        from Malaysia. Before I continue to introduce myself, this website does
+        not only act as my portfolio but I hope to make new friends through here
+        (which explains my hobby section =D).{" "}
+      </p>
+      <p>
+        I graduated from University of Manchester under the degree of Politics,
+        Philosophy and Economics, focusing more on Philosophy and Economics.
+        During my studies, I took an online course focusing on front-end
+        development and has mastered HTML, CSS, JavaScript and React.js.
+        Throughout the online course, I was able to design clock app, weather
+        app, a dictionary and simple websites. I also learned Figma and honed my
+        skill on using it though designing my own websites (though I prefer
+        wireframes more). I would say my work flow is more flexible and I work
+        quicker when using wireframes. I have yet to specialise in any industry
+        but I imagine my forte will be in helping others in designing their
+        portfolio and writing. I am sure that with my skills, I will be able to
+        understand your idea and materialise it.
+      </p>
+      <h3>Skills:</h3>
+      <p>
+        Technical Skills: HTML, CSS, JavaScript, React.js, Figma, Canva, GitHub,
+        Netlify
+      </p>
+      <p>
+        Other relavant skills: Communication skills, Problem Solving Skills,
+        Creative Writing Skills, Creativity and Design
+      </p>
+      <h3>Spoken Languages:</h3>
+      <p>
+        As someone who loves languages, I am able to speak 3 languages fluently
+        and 3 languages intermediately. With this, you can speak comfortably
+        with me in the language you desire in order to get the idea across.
+        (Lower language barrier vibes)
+      </p>
+      <p>Fluent in speaking and reading: Malay, English and Mandarin</p>
+      <p>
+        Intermediate in speaking and reading: Cantonese, Korean and Japanese
+      </p>
+      <h3>My Hobbies {">.<"}:</h3>
+      <p>
+        As a curious person, I tend to try many things as I grew up and only
+        some hobbies are stuck with me now. I have a few hobbies so hang in
+        there for me.
+      </p>
+      <p>
+        {" "}
+        My first hobby that stuck till now is crocheting. I enjoy very much in
+        crocheting amigurumi and seeing that the pattern work making me feel
+        very achieved. On top of that, I also tend to change the patterns to
+        make the amigurumi into my own style (it is my own way to express my
+        creativity).{" "}
+      </p>
+      <p>
+        My second hobby is Kyudo which is more commonly understood as Japanese
+        traditional archery. I pick up this sport (or mobile meditation) during
+        my degree. As I was stressing out trying to write my assignments, my
+        sister decided to drag me out in the sun and to go watch Kyudo in
+        action. There are no words to describe the moment of when I fell in love
+        with the sport. Different from modern archery, Kyudo actually has a
+        whole manual from how you stand until how you release the arrow. Its a
+        perfect sport for me as each steps were clear and I do not have to think
+        to much on the basic skills (which helps me a lot in enjoying the
+        sport).
+      </p>
+      <p>
+        {" "}
+        When I was younger, I used to hate reading as it made be sleepy every
+        time and writing essays was not my forte. After my degree, I sort of
+        turned the habit of completing reading lists and writing assignments
+        into a hobby of mine. Nowadays, reading brings me joys and fulfil my
+        curiosity rather than making me fall asleep. (However, reading before
+        sleeping still helps {">.<"}). Journaling and writing used to mean
+        nothing to me as I believe that it brings me nothing and cause waste of
+        papers. These habits became something that I need as I grew up as there
+        are a lot I have in mind and it is unnecessarily that I am able to find
+        someone to talk to (and dump it onto them, which makes me feel bad).
+      </p>
+      <p>
+        {" "}
+        Contrasting from writing not being my forte in the past, I have always
+        excelled anything involving numbers and logical thinking. It was easier
+        for my brain gear to turn when faced with these problems which helps me
+        in earning good grades. (I used to really love mathematic homework and
+        spend more time on them compared to other subjects). Related to my
+        degree and interest, I became passionate in data organising and actually
+        make use of it. This passion also has let me into discovering that I
+        have so much free will that I should make use of. My mother owns a
+        textile business and she often needs help in uploading lots of cash
+        sales at once. At times, I was frustrated as when she assigned me the
+        tasks, she would as me to upload them one by one which takes up so much
+        time. That’s when I took some time and think hard on how to shorten the
+        time which led me to use my free will to use SQL. As someone who
+        self-learned SQL, I feel achieved every time I extract data from raw
+        files and get the same total amount as the needed amount. So I realise
+        data organising is one of my passion.
+      </p>
+      <p>
+        {" "}
+        Lastly, I love learning languages. It’s a wonderful thing that human has
+        invented as we are able to express our minds which was innate to the
+        external world. As someone who had a hard time in expressing and more
+        often unheard, learning language became my favourite past time as for me
+        language is so important for me to understand others and for others to
+        understand me. As this section became to long, I have decided to cut
+        short.
       </p>
     </div>
   );
