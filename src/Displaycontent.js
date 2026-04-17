e619f5851c83482fe9ec5c9419c6f1538ee091f2
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-17 11:49:35 +0800

    Changed fonts for title and navigations

--- src/Displaycontent.js
+++ src/Displaycontent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import About from "./About.js";
 import Education from "./Education.js";
 import Showcerts from "./Showcertificates.js";
@@ -23,7 +24,7 @@ export default function DisplayContent(props) {
   } else {
     return (
       <div>
-        <h1 className="p-3 m-3">
+        <h1 className="p-3 m-3 prompt">
           Hi 👋🏻 Welcome to my portfolio. Click any button at the side to begin!
         </h1>
       </div>
