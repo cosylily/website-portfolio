620667e199cddaa1623cd0746a727196007f28c5
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-20 20:29:25 +0800

    Added a video for the first website
    Added a video of showcasing the website

--- src/Websites.js
+++ src/Websites.js
@@ -5,7 +5,30 @@ export default function displaywebsites() {
   return (
     <div>
       <h2>Websites</h2>
-      <p>Descriptions and challenges of coding each websites</p>
+      <p>Here are three websites that I coded recently:</p>
+      <div className="lifeupdate">
+        <h3>Sharing life update with friends:</h3>
+        <video
+          src="/lifeupdate.mov"
+          width="50%"
+          height="auto"
+          autoPlay
+          loop
+          muted="true"
+          playsinline
+        ></video>
+        <p>
+          As we live far away from each other, my friend has organised a meeting
+          for us to gather online and present our life updates. I thought to
+          myself that creating a presentation would be cliche, so I created a
+          website instead {">.<"}
+        </p>
+        <div className="webbuttons">
+          {" "}
+          <button>Learn more</button>
+          <a href="https://syafsupdate.netlify.app/">Visit Website</a>
+        </div>
+      </div>
     </div>
   );
 }
