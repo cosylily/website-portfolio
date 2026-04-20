299deef870b7b05d9ffa7f6d4164ad7c56f849a0
cosylily <syafiqah.dayana1205@gmail.com>
2026-04-20 16:28:13 +0800

    Added images into showcertificates.js
    To display certificates in the webpage. Also add a new css file to avoid the difficulty of searching the classNames later on

--- src/Showcertificates.js
+++ src/Showcertificates.js
@@ -1,9 +1,37 @@
 import React from "react";
+import "./App.css";
+import "./certificates.css";
 
 export default function Certs() {
   return (
     <div>
       <h2>Certifications of Coding Skills</h2>
+      <div className="certificates">
+        <img
+          src="/cert1.png"
+          alt="Introduction to Coding Skills Certificate of completion."
+        />
+        <img
+          src="/cert3.png"
+          alt="Web Development Certificate of completion."
+        />
+        <img
+          src="cert4.png"
+          alt="Introduction to Python Development Certificate of completion"
+        />
+        <img
+          src="cert5.png"
+          alt="Advanced Python Development Certificate of completion"
+        />
+        <img
+          src="cert 6.png"
+          alt="Responsive Web Development Certificate of Completion"
+        />
+        <img
+          src="cert7.png"
+          alt="Advanced Responsive Web Development Certificate of Completion"
+        />
+      </div>
     </div>
   );
 }
