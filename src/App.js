fa3d56b076b8e7bd76b7b885441ab58eecb8ad33
cosylily <syafiqah.dayana1205@gmail.com>
2026-05-23 18:28:44 +0100

    Restart
    Cleared everything and restart again. Start with the toppage to welcome the people into my website. changing the theme of the website. added a new background

--- src/App.js
+++ src/App.js
@@ -1,76 +1,22 @@
 import "./App.css";
-import Footerbottom from "./Bottompage.js";
-import Content from "./Displaycontent.js";
-import { useState } from "react";
-
 function App() {
-  let [content, setContent] = useState();
-
   return (
     <div className="App">
-      <header className="title" id="toppage">
-        <h2>Syafiqah's</h2>
-        <div>
+      <div className="toppage">
+        <h2 className="greeting">Greetings! Welcome to </h2>
+        <div className="title">
+          <h2>Syafiqah's</h2>
           <img
-            src="logoforportfolio.jpg"
-            alt="Self-doodle logo"
+            src="/logoforportfolio.jpg"
+            alt="My own logo doodling"
             height="100px"
             width="auto"
           />
-        </div>
-        <h2>Portfolio</h2>
-      </header>
-      <div className="canvas">
-        <div className="buttons">
-          <div className="buttonsinbuttons">
-            <button onClick={changeHome} className="homebutton">
-              Home
-            </button>
-            <button onClick={changeAbout} className="aboutbutton">
-              About
-            </button>
-            <button onClick={changeEducation} className="edubutton">
-              Education
-            </button>
-            <button onClick={changeCertificate} className="certbutton">
-              Certificate
-            </button>
-            <button onClick={changeWebsites} className="webbutton">
-              Websites
-            </button>
-            <button onClick={changeContact} className="contactbutton">
-              Contact
-            </button>
-          </div>
-        </div>
-
-        <div className="emptyspace">
-          <div className="introduction anchor">
-            <Content name={content} />
-          </div>
+          <h2>Portfolio</h2>
         </div>
       </div>
-      <Footerbottom />
     </div>
   );
-  function changeHome() {
-    setContent("Home");
-  }
-  function changeAbout() {
-    setContent("about");
-  }
-  function changeEducation() {
-    setContent("education");
-  }
-  function changeCertificate() {
-    setContent("certificates");
-  }
-  function changeWebsites() {
-    setContent("websites");
-  }
-  function changeContact() {
-    setContent("contacts");
-  }
 }
 
 export default App;
